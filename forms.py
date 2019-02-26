e59f8f943704f85cd65d0d2d93771bbe8d2d223d
annika <annikaslund@gmail.com>
2019-02-26 12:27:00 -0800

    Add PasswordField to Registeruserform

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, PasswordField
 from wtforms.validators import Length, Optional, Email
 
 
@@ -8,10 +8,10 @@ class RegisterUserForm(FlaskForm):
 
     username = StringField("Username",
                            validators=[Length(min=1, max=20, message="Username must be between 1 and 20 characters.")],)
-    password = StringField("Password",
-                           validators=[Length(min=1, message="Must put in a password.")])
+    password = PasswordField("Password",
+                             validators=[Length(min=1, message="Must put in a password.")])
     email = StringField("Email",
-                        validators=[Email(), Length(min=1, max=50, 
+                        validators=[Email(), Length(min=1, max=50,
                                                     message="Email must be between 1 and 50 characters.")])
     first_name = StringField("First name",
                              validators=[Length(min=1, max=30, message="First name must be between 1 and 30 characters.")])
