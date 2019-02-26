e5ab5ab012f20be83f8c6904e31e8b79b535c122
annika <annikaslund@gmail.com>
2019-02-26 12:15:56 -0800

    Add register form in templates folder

--- app.py
+++ app.py
@@ -7,9 +7,35 @@ from models import User, connect_db, db
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = "abc123"
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgres:///adoption_agency"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgres:///flask_feedback"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 debug = DebugToolbarExtension(app)
 
 connect_db(app)
+
+
+@app.route('/')
+def redirect_to_register():
+    """ Redirects to register page """
+
+    return redirect('/register')
+
+
+@app.route('/register')
+def register_user():
+    """ Show register form, handles registering of users. """
+
+    form = AddSnackForm()
+
+    if form.validate_on_submit():
+        # import pdb
+        # pdb.set_trace()
+        name = form.name.data
+        price = form.price.data
+        return f"Add {name} at {price}"
+        # redirect("/")
+
+    else:
+        return render_template(
+            "snack_add_form.html", form=form)
--- models.py
+++ models.py
@@ -1,6 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 db = SQLAlchemy()
+bcrypt = Bcrypt()
 
 
 def connect_db(app):
@@ -11,7 +13,7 @@ def connect_db(app):
 
 class User(db.Model):
     """ User model """
-
+     
     __tablename__ = "users"
 
     username = db.Column(db.String(20),
@@ -29,3 +31,12 @@ class User(db.Model):
 
     last_name = db.Column(db.String(30),
                           nullable=False,)
+
+    @classmethod
+    def register(cls, username, pwd):
+        """ Returns instance of user with username and encrypted password """
+        hashed = bcrypt.generate_password_hash(pwd)
+
+        hashed_utf8 = hashed.decode("utf8")
+
+        return cls(username=username, password=hashed_utf8)
